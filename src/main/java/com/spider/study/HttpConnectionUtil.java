da4fb1f99feb0f9eee5a6e7342779c8167311c51
wuhaisheng <15001167619@163.com>
2019-02-12 10:24:04 +0800

    文件下载

--- src/main/java/com/spider/study/HttpConnectionUtil.java
+++ src/main/java/com/spider/study/HttpConnectionUtil.java
@@ -27,47 +27,25 @@ public class HttpConnectionUtil {
     public static File downloadFile(String urlPath, String downloadDir) {
         File file = null;
         try {
-            // 统一资源
-            URL url = new URL(urlPath);
-            // 连接类的父类，抽象类
-            URLConnection urlConnection = url.openConnection();
-            // http的连接类
+            URLConnection urlConnection = new URL(urlPath).openConnection();
             HttpURLConnection httpURLConnection = (HttpURLConnection) urlConnection;
-            // 设定请求的方法，默认是GET
-            httpURLConnection.setRequestMethod("GET");
-            // 设置字符编码
             httpURLConnection.setRequestProperty("Charset", "UTF-8");
-            // 打开到此 URL 引用的资源的通信链接（如果尚未建立这样的连接）。
             httpURLConnection.connect();
-
-            // 文件大小
-            int fileLength = httpURLConnection.getContentLength();
-
-            // 文件名
             String filePathUrl = httpURLConnection.getURL().getFile();
             String fileFullName = filePathUrl.substring(filePathUrl.lastIndexOf(File.separatorChar) + 1);
-
-            System.out.println("file length---->" + fileLength);
-
-            URLConnection con = url.openConnection();
-
             BufferedInputStream bin = new BufferedInputStream(httpURLConnection.getInputStream());
-
             String path = downloadDir + File.separatorChar + fileFullName;
             file = new File(path);
             if (!file.getParentFile().exists()) {
                 file.getParentFile().mkdirs();
             }
             OutputStream out = new FileOutputStream(file);
-            int size = 0;
+            int size;
             int len = 0;
             byte[] buf = new byte[1024];
             while ((size = bin.read(buf)) != -1) {
                 len += size;
                 out.write(buf, 0, size);
-                // 打印下载百分比
-                // System.out.println("下载了-------> " + len * 100 / fileLength +
-                // "%\n");
             }
             bin.close();
             out.close();
@@ -84,9 +62,9 @@ public class HttpConnectionUtil {
     }
 
     public static void main(String[] args) {
-
         // 下载文件测试
-        downloadFile("https://201712mp4.89soso.com/20171208/15/1/xml/91_95d9047eefc941efaef7c54fb2899e41.mp4", "G:\\whs\\files");
+        downloadFile("https://201712mp4.89soso.com/20171208/15/1/xml/91_95d9047eefc941efaef7c54fb2899e41.mp4",
+                "G:\\whs\\files");
 
     }
 
